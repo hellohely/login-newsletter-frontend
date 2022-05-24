b6c81dd7f2f18963c64f62013975417735bb6739
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-05-24 22:54:57 +0200

    Posting new users to mongodb database.
    Only works with crome plugin moseif, otherwise I get CORS errors

--- login-newsletters/src/components/signup.tsx
+++ login-newsletters/src/components/signup.tsx
@@ -1,15 +1,49 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
+
 export function SignUp() {
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const headers = { 
+      Accept: 'application/json',
+    'Content-Type' : 'application/json',
+};
+
   return (
-    <div>
-      <form>
-        <h1>Registrera ny användare</h1>
-        Förnamn: <input type={"text"}></input>
-        Efternamn: <input type={"text"}></input>
-        Email: <input type={"email"}></input>
-        Önskat lösenord:<input type={"password"}></input>
-        Ja, jag vill ta emot nyhetsbrev<input type={"checkbox"}></input>
-        <input type={"submit"} value="Registrera"></input>
-      </form>
-    </div>
+      //spara användarens inputs till ett objekt
+    <form
+      onSubmit={handleSubmit((data) => {
+        console.log(data);
+        //fixa cors-fel, behöver headers! //works with chrome plugin moesif
+        axios.post('http://localhost:3000/add', data, { headers })
+        .then(function(response){
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+          });
+      })}
+    >
+      <h1>Registrera ny användare</h1>
+      <label htmlFor="firstName">Förnamn:</label>
+      <input {...register("firstName")} />
+      <input {...register("lastName")} />
+      <input {...register("email")} />
+      <input type="password" {...register("password")} />
+      <input type="submit" />
+    </form>
   );
 }
+
+
+
+
+
+//Användarens unika ID?
