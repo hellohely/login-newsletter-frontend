1481f4f9ff6826dbaa9249c2ff7d262c127dc54a
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-05-24 20:24:59 +0200

    Created form in signup

--- login-newsletters/src/components/signup.tsx
+++ login-newsletters/src/components/signup.tsx
@@ -1,3 +1,15 @@
 export function SignUp() {
-    return (<div><h1>Registrera ny användare</h1></div>)
+  return (
+    <div>
+      <form>
+        <h1>Registrera ny användare</h1>
+        Förnamn: <input type={"text"}></input>
+        Efternamn: <input type={"text"}></input>
+        Email: <input type={"email"}></input>
+        Önskat lösenord:<input type={"password"}></input>
+        Ja, jag vill ta emot nyhetsbrev<input type={"checkbox"}></input>
+        <input type={"submit"} value="Registrera"></input>
+      </form>
+    </div>
+  );
 }
