766bd7c1f40448ca123c9b65544adcc93f510b25
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-05-27 09:35:53 +0200

    Sets user id cookie in frontend

--- login-newsletters/src/components/login.tsx
+++ login-newsletters/src/components/login.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useForm } from "react-hook-form";
+import Cookies from "universal-cookie";
+
 
 type FormValues = {
   email: string;
@@ -8,36 +10,49 @@ type FormValues = {
 
 export function LogIn() {
   const { register, handleSubmit } = useForm<FormValues>();
+  const cookies = new Cookies();
 
   const headers = {
     Accept: "application/json",
     "Content-Type": "application/json",
+    withCredentials: true
   };
 
   return (
     //Create object with logon credentials
     <form
       onSubmit={handleSubmit((data) => {
-        console.log(data);
+        //console.log(data);
         //Send object to Express server
 
         axios
           .post("http://localhost:3000/login", data, { headers })
           .then(function (response) {
+              //let kaka = response.headers["set-cookie"]
             console.log(response);
+            
             if (response.status === 200) {
-                return console.log("Anv inloggad");   
+                //Server responds with user ID. Need to save user ID to cookies
+                //let kaka: any = cookies.get("kaka");
+                //console.log(kaka);
+                
+                console.log(response.data);
+
+                cookies.set('userID', response.data.id)
+                console.log(cookies.get('userID'));
+                
+                
+                
+              return console.log("Anv inloggad");
             }
-                console.log("Okänt fel");
+            console.log("Okänt fel");
           })
           .catch(function (error) {
             console.log(error);
             if (error.code === "ERR_BAD_REQUEST") {
-                return console.log("Felaktigt anv el lösen");
+              return console.log("Felaktigt anv el lösen");
             }
-                console.log("Okänt fel");
-                
-
+            console.log("Okänt fel");
           });
       })}
     >
